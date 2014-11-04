4d2e9e475e7697fcfcfb89a573c2f8f5509a90b9
coldwellop1 <coldwellop1@users.noreply.github.com>
2014-11-04 13:09:21 -0500

    Update populate.js

--- hw14/populate.js
+++ hw14/populate.js
@@ -4,18 +4,6 @@ var Ponies = require('mongodb');
 // Connect to the db
 Ponies.connect("mongodb://erose1:standard@ds051160.mongolab.com:51160/comp398_hw_14", function(err, db) {
   if(err) { return console.dir(err); }
-  /*
-  var Anat = ["A","B","C#","D","E","F#","G#"];
-  var Bflat = ["Bb","C","D","Eb","F","G","A"];
-  var Bnat = ["B","C#","D#","E","F#","G#","A#"];
-  var Cnat = ["C","D","E","F","G","A","B"];
-  var Csharp = ["C#","D#","E#","F#","G#","A#","B#"];
-  var Dnat = ["D","E","F#","G","A","B","C#"];
-  var Eflat = ["Eb","F","G","Ab","Bb","C","D"];
-  var Enat = ["E","F#","G#","A","B","C#","D#"];
-  var Fnat = ["F","G","A","Bb","C","D","E"];
-  var Fsharp = ["F#","G#","A#","B","C#","D#","E#"];
-  */
   
   var Anat = "A B C# D E F# G#";
   var Bflat = "Bb C D Eb F G A";
@@ -43,6 +31,6 @@ Ponies.connect("mongodb://erose1:standard@ds051160.mongolab.com:51160/comp398_hw
 	collection.insert(MajScales, function(err, result){});
   }
   
-  //console.log("Collection Populated!");
+  console.log("Collection Populated!");
   
 });
