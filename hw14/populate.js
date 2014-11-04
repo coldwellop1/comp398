fd2bb85a5ba67c2752c90d45a0345040aa0e6aa0
coldwellop1 <coldwellop1@users.noreply.github.com>
2014-11-04 10:48:09 -0500

    Update populate.js

--- hw14/populate.js
+++ hw14/populate.js
@@ -4,9 +4,20 @@ var Ponies = require('mongodb').MongoClient;
 
 // Connect to the db
 Ponies.connect("mongodb://erose1:standard@ds051160.mongolab.com:51160/comp398_hw_14", function(err, db) {
-/*  if(err) { return console.dir(err); }
+  if(err) { return console.dir(err); }
+  
+  var Anat = ["A","B","C#","D","E","F#","G#"];
+  var Bflat = ["Bb","C","D","Eb","F","G","A"];
+  var Bnat = ["B","C#","D#","E","F#","G#","A#"];
+  var Cnat = ["C","D","E","F","G","A","B"];
+  var Csharp = ["C#","D#","E#","F#","G#","A#","B#"];
+  var Dnat = ["D","E","F#","G","A","B","C#"];
+  var Dsharp = ["","","","","","",""];
+  var Enat = ["E","F#","G#","A","B","C#","D#"];
+  var Fnat = ["F","G","A","Bb","C","D","E"];
+  var Fsharp = ["F#","G#","A#","B","C#","D#","E#"];
 
-  var collection = db.collection('test');
+/*  var collection = db.collection('test');
   var docs = [{mykey:1}, {mykey:2}, {mykey:3}];
 
   collection.insert(docs, {w:1}, function(err, result) {
